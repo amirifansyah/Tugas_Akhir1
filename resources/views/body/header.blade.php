03d80e15ddabff62da4d9822001236c6147e1a0b
amirifansyah <amirifansyah5@gmail.com>
2021-06-02 14:12:46 +0700

    memasukan text

--- resources/views/body/header.blade.php
+++ resources/views/body/header.blade.php
@@ -5,7 +5,7 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
     </ul>
-
+    <P>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil consectetur repellendus voluptatum maiores ratione in corporis dolores! Enim quibusdam a laboriosam dolores, nobis placeat nesciunt quos commodi, veritatis earum atque!</P>
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
       <!-- Navbar Search -->
