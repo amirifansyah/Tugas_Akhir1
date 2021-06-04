c7031238963993d60fbc5e18c9789fc9451a16d0
amirifansyah <amirifansyah5@gmail.com>
2021-06-04 12:39:58 +0700

    merapihkan header

--- resources/views/body/header.blade.php
+++ resources/views/body/header.blade.php
@@ -5,6 +5,10 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
     </ul>
+    <a href="{{route('pasiens.index')}}" class="brand-link">
+      <img src="{{ asset('AdminLTE/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">RS Aman Jaya</span>
+    </a>
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
       <!-- Navbar Search -->
@@ -38,10 +42,5 @@
         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
            @csrf
         </form>
-      <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-          <i class="fas fa-th-large"></i>
-        </a>
-      </li>
     </ul>
   </nav>
